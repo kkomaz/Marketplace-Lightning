34054433b9ad824421aa6883c6b32ccc86a67c64
Alex Lee <alex@uniteus.com>
2019-06-17 12:42:25 -0400

    Clinet Work

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const clickMe = async () => {
     const items = await axios.get('http://localhost:5000/api/items')
     console.log(items)
   }
+
   return (
     <div className="App">
-      <button onClick={clickMe}>
-        Click me
-      </button>
+      <Button variant="primary" onClick={clickMe}>
+        Click Me
+      </Button>
     </div>
   );
 }
