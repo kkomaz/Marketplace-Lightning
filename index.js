1a251140102dc478e82fc34804f926965311329b
Alex Lee <alex@uniteus.com>
2019-06-16 23:01:09 -0400

    Create

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys.js')
 require('./model/Item');
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 require('./routes/ItemRoutes')(app);
 
--- routes/ItemRoutes.js
+++ routes/ItemRoutes.js
@@ -2,6 +2,29 @@ const mongoose = require('mongoose');
 const Item = mongoose.model('items');
 
 module.exports = app => {
+  app.post('/items', async (req, res) => {
+    if (!req.body.description) {
+      return res.status(400).send({
+        message: 'Item content can not be empty'
+      })
+    }
+
+    const item = new Item({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+    })
+
+    try {
+      const data = await item.save()
+      res.send(data)
+    } catch (e) {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Item."
+      });
+    }
+  })
+
   app.get('/items', async (req, res) => {
     try {
       const items = await Item.find()
